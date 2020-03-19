19c297ed7ac636e18b02994fabb706a14681680e
seeratalishah <seeratalykhan@gmail.com>
2020-03-19 14:04:03 +0500

    XML done

---
+++ app/src/main/java/com/example/shiningstar/Login_Admin.java
@@ -0,0 +1,14 @@
+package com.example.shiningstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__admin);
+    }
+}
---
+++ app/src/main/java/com/example/shiningstar/Register_Staff.java
@@ -0,0 +1,14 @@
+package com.example.shiningstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Staff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__staff);
+    }
+}
