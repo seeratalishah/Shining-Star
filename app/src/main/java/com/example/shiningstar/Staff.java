2d9bb7a20157d504124b3540c09f89429f21f65d
seeratalishah <seeratalykhan@gmail.com>
2020-03-20 15:49:06 +0500

    Staff design

--- app/src/main/java/com/example/shiningstar/Staff.java
+++ app/src/main/java/com/example/shiningstar/Staff.java
@@ -3,12 +3,26 @@ package com.example.shiningstar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Staff extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff);
+
     }
+
+
 }
