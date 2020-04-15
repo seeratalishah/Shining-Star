86e1aa609de82b0f6308d134ea424001de1c7338
seeratalishah <seeratalykhan@gmail.com>
2020-04-15 15:17:59 +0500

    Revert "Parent Navigation"
    This reverts commit bf079e1ebee3b7c7703096e045b6d5d31cf31aec.

--- app/src/main/java/com/example/shiningstar/ParentDashboard.java
+++ app/src/main/java/com/example/shiningstar/ParentDashboard.java
@@ -1,24 +1,17 @@
 package com.example.shiningstar;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,7 +19,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ParentDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ParentDashboard extends AppCompatActivity {
 
 
     TextView name_parent;
@@ -59,40 +52,7 @@ public class ParentDashboard extends AppCompatActivity implements NavigationView
         logout = findViewById(R.id.logoutbtn);
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_parent);
-        toolbar.setTitle("Shining Star");
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener (this);
-
-        final View hView =  navigationView.getHeaderView(0);
-        firebaseAuth = FirebaseAuth.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-        String email_curr = firebaseAuth.getCurrentUser().getEmail();
-        String uid = firebaseAuth.getCurrentUser().getUid();
-        databaseReference.child("parents").child(uid).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                TextView nav_username = (TextView) hView.findViewById(R.id.name_parent);
-                String name_small = dataSnapshot.getValue().toString();
-                String name_big = name_small.substring(0, 1).toUpperCase() + name_small.substring(1);
-                nav_username.setText(name_big);
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        TextView nav_email = (TextView) hView.findViewById(R.id.email_parent);
-        nav_email.setText(email_curr);
-
+        getData();
 
 
         add_child.setOnClickListener(new View.OnClickListener() {
@@ -126,27 +86,51 @@ public class ParentDashboard extends AppCompatActivity implements NavigationView
             }
         });
 
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PrefManager pref = new PrefManager(getApplicationContext());
+                pref.resetData();
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+
+
+
+
     }
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.logout_parent) {
-            PrefManager pref = new PrefManager(this);
-            pref.resetData();
-            FirebaseAuth.getInstance().signOut();
-            Intent i = new Intent(this,MainActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);
-            finish();
-        }
+    private void getData(){
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        String email_curr = firebaseAuth.getCurrentUser().getEmail();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        databaseReference.child("parents").child(uid).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name_small = dataSnapshot.getValue().toString();
+                String name_big = name_small.substring(0, 1).toUpperCase() + name_small.substring(1);
+                name_parent.setText(name_big);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        email_parent.setText(email_curr);
+
     }
-
 
 }
